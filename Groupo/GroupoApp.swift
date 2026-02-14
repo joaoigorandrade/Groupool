06b378187d1da1295fa5717078a863bd2adda90f
Joao Igor <joaoigora@gmail.com>
2026-02-14 18:04:17 -0300

    Initial Commit

---
+++ Groupo/GroupoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupoApp.swift
+//  Groupo
+//
+//  Created by Joao Igor de Andrade Oliveira on 14/02/26.
+//
+
+import SwiftUI
+
+@main
+struct GroupoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
